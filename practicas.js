e1107f2d5f1634af1e1cf804c13e1816c84a8f7e
micky rendon <elderendon@hotmail.es>
2021-04-16 23:59:16 -0300

    funciones que validan el spacebar

--- practicas.js
+++ practicas.js
@@ -1,51 +1,18 @@
 // usuario y contrasena
-// crear una funcion que guarde los valores ingresados de los prompt
 //crear otra funcion para validar los datos ingresados
+
 // let minChar  = ' ';
 
-// funcion para capturar la tecla presionada
+// variables globales
 let inputs = document.getElementsByTagName('INPUT');
-let inputLastN  = '';
 
 // obtiene su valor luego de invocar a 'arrayInputs()'
 let data1;
 let consoleMsg;
 let lastValue = [];
-const arrayInputs = () => {
-    //guardando datos en un array
-    let data = [];
-
-    let inputsValue = inputs;
-
-    for (let i = 0; i < inputs.length; i++) {
-        // definir si trim es necesario para el valor de los inputs o directamente hacer una validacion para que no se acepten caracteres con espacio dentro del input
-        data.push(inputsValue[i].value.trim());
-
-    }
-    // return inputsValue;
-    data1 = data;
-
-    // funciona!...desestimar el input de submit
-    
-    for(i = 0; i < data1.length; i++){
-        data1[i].length == 0 ? console.log('igual que 0') :
-        (lastValue.push(data1[i]),console.log('mayor que 0'));
-    }
-    // mostrando los resultados por consola
-    console.log('valores de los inputs ' + lastValue);
-    // console.log(`valores de los inputs ${lastValue}`);
-    consoleMsg = console.table(lastValue);
-
-    return data;
-}
-// invocacion de prueba, en html descomentar el submit para volverlo a la normalidad y que funcione el evento onclick de dicho boton
-   arrayInputs() 
 
 
-
-
-
-
+// capturar las teclas presionadas
 function onKeyDown(event) {
     // captura el nombre de cada tecla
     const key = event.key;
@@ -55,34 +22,62 @@ function onKeyDown(event) {
 
     // es camelcase
     if(key == 'Enter'){
-                
+           
         return console.log('tecla Enter presionada');
     }
-
-    //capturando el valor de 'spacebar', despues de mostrar el alert limpiar el input
-    codigo == 32 ? (alert('prohibido ingresar espacios en blanco'), ) :
+    
+    //capturando el valor de 'spacebar'.
+    codigo == 32 ? (alert('prohibido ingresar espacios en blanco'), cleaner() ):
     console.log('teclas correctas');
 };
 
+// funcion para agregar los valores de los inputs a un array
+const arrayInputs = () => {
+    //guardando datos en un array
+    let data = [];
+    let inputsValue = inputs;
 
-// rest
-function rest(...argument){
-    let array1 = argument;
+    //pusheando el valor de cada elemento del formulario
+    for (let i = 0; i < inputs.length; i++) {
 
-    console.log(array1);
+        data.push(inputsValue[i].value.trim());
+    }
+    data1 = data;
+    arrayLength();
+
+    return data1;
 }
-// encontrar como pasarle los datos del array de la variable 'array inputs' 
-let parametros = rest();
-// 
+// invocacion de prueba, en html descomentar el submit para volverlo a la normalidad y que funcione el evento onclick de dicho boton
+   arrayInputs() 
 
-// function cleanForm(){
-//     var element = document.getElementsByTagName('input');
-    
-//     if()
-//     element.settAtribute('auto-complete', '')
-    
-// }
 
+function arrayLength(){
+    // ciclo para agregar al array(lastValue) los elementos del array(data1) que su length es mayor que 0 o que no esten vacios.
+    for(i = 0; i < data1.length; i++){
+        data1[i].length == 0 ? console.log('valores vacios') :
+        (lastValue.push(data1[i]),console.log(`valor ${lastValue[i]}`));
+    }
+}
+
+
+
+// VALIDADORES
+
+// limpia el input que tiene focus en ese momento despues de clickear el spacebar...
+let inputLastN  = '';
+
+function cleaner(){
+    // falla la primera vez, despues funciona correctamente
+    for (let i = 0; i < inputs.length; i++) {
+        inputs[i].addEventListener("focus", function() {
+        // Guardamos la ID del elemento al que hacemos 'focus'
+        inputLastN = this;
+        console.log('elemento focus');
+        });
+    };
+
+    return inputLastN.value = ''
+}
 
 // validador de valores  MEJORAR
 function validator(){
@@ -107,12 +102,16 @@ function validator(){
 
 
 
-//     // if user is NaN / 
 }
 // validator();
 
 
 // submit onclik
 function sending(){
+    
+    lastValue.length == 0 ? alert('por favor complete el formulario'):
+    (consoleMsg = console.table(lastValue),
+    console.log('valores de los inputs ' + lastValue));
+
     alert(`valores obtenidos del formulario \n ${arrayInputs()}`)
-}
+};
