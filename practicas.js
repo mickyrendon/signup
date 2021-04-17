d5c61dc8b30a2a211d00889bab02e3b4b52a9168
micky rendon <elderendon@hotmail.es>
2021-04-17 10:29:49 -0300

    verificando funciones cleaner y arrayInputs

--- practicas.js
+++ practicas.js
@@ -29,20 +29,23 @@ function onKeyDown(event) {
     }
     
     //capturando el valor de 'spacebar'.
-    codigo == 32 ? (alert('prohibido ingresar espacios en blanco'), cleaner() ):
-    console.log('teclas correctas');
+    if(codigo == 32){
+        alert('prohibido ingresar espacios en blanco');
+        cleaner(); 
+    };
 };
 
 // limpia el input que tiene focus en ese momento despues de clickear el spacebar...
 let inputLastN  = '';
 
+// verificar que funcione
 function cleaner(){
     // falla la primera vez, despues funciona correctamente
     for (let i = 0; i < inputs.length; i++) {
         inputs[i].addEventListener("focus", function() {
         // Guardamos la ID del elemento al que hacemos 'focus'
         inputLastN = this;
-        console.log('elemento focus');
+        console.log('elemento focus');     
         });
     };
 
@@ -54,20 +57,31 @@ const arrayInputs = () => {
     //guardando datos en un array
     let data = [];
     let inputsValue = inputs;
+    let inptsRecorrido;
 
     //pusheando el valor de cada elemento del formulario
     for (let i = 0; i < inputs.length; i++) {
 
         data.push(inputsValue[i].value.trim());
+
+    // validador de < 2 para los caracteres ingresados TERMINAR!.. funciona pero falta pulirlo, despues de mostrar el alert ver si conviene limpiar los campos o no...
+        inptsRecorrido = inputsValue[i].value;
+    
+        
     }
+
+    inptsRecorrido.length <= 2 ?(
+        console.log('ingresa dos o mas caracteres po'), alert('ingrese bien los datos culiao')
+        ) : (
+            console.log('bien hecho, ingresaste mas de dos caracteres')
+        );
+
+
     data1 = data;
     arrayLength();
 
     return data1;
 }
-// invocacion de prueba, en html descomentar el submit para volverlo a la normalidad y que funcione el evento onclick de dicho boton
-   arrayInputs() 
-
 
 function arrayLength(){
     // ciclo para agregar al array(lastValue) los elementos del array(data1) que su length es mayor que 0 o que no esten vacios.
@@ -79,39 +93,38 @@ function arrayLength(){
 
 // VALIDADORES
 
-// validador de valores  MEJORAR
 function validator(){
     // event.preventDefault()
 
     // validando que el campo no esté vacio
     // variable de multiples valores
     // CREAR EL MAQUETADO
-    var inputRequires = document.getElementsByTagName('input').value;
+    // var inputRequires = inputs.value;
 
-    inputRequires == "" ?
+    /* inputRequires == "" ?
         alert(`ingrese el ${inputRequires}`) 
-        : console.log('datos validos');
+        : console.log('datos validos'); */
 
     // validando la cantidad de caracteres ingresados
-    user.length <= 2 || password.length <= 2 ?(
-            console.log('ingresa dos o mas caracteres po'), alert('ingrese bien los datos')
-        ) : (
-            console.log('datos correctos')
-        );
+    
 
 
 
 
 }
-// validator();
 
 
 // submit onclik
 function sending(){
     
+    // validator();
+        
     lastValue.length == 0 ? alert('por favor complete el formulario'):
     (consoleMsg = console.table(lastValue),
     console.log('valores de los inputs ' + lastValue));
 
+    // codigo solo para probar que los datos del formulario se guardan en un array  y se muestran en un alert
     alert(`valores obtenidos del formulario \n ${arrayInputs()}`)
+
+
 };
